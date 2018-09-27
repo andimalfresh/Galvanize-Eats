84aa12a2bfd043ed5fed7f7274ab7a0df7241b2d
andimalfresh <w.andrew.pedersen@gmail.com>
2018-09-27 11:28:13 -0600

    Successfully Grabbing infomation from API

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+
+document.addEventListener("DOMContentLoaded", function printNames(event) {
+    fetch('https://galvanize-eats-api.herokuapp.com/menu')
+        .then((data) => data.json())
+        .then((data) => {
+            document.getElementsByClassName('.foodAndPriceList')
+            for (var i = 0; i < data.menu.length; i++) {
+                console.log(data.menu[i].name)
+            }
+
+            for (var i = 0; i < data.menu.length; i++) {
+                console.log(data.menu[i].price)
+            }
+        })
+})
+
 fetch('https://galvanize-eats-api.herokuapp.com/menu')
     .then((data) => data.json())
-    .then((data) => console.log(data))
+    .then((data) => function getNames(data) {
+        for (var i = 0; i < data.menu.length; i++) {
+            console.log(data.menu[1].name)
+        }
+    })
