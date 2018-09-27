4fd992de6d50f04c6d810da94cef8ba0b16f6dba
andimalfresh <w.andrew.pedersen@gmail.com>
2018-09-27 14:54:07 -0600

    Got API Info passed into page thru Dom (Finally)

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
 
-document.addEventListener("DOMContentLoaded", function printNames(event) {
+
+document.addEventListener("DOMContentLoaded", function printNames(pushNames) {
     fetch('https://galvanize-eats-api.herokuapp.com/menu')
         .then((data) => data.json())
         .then((data) => {
-            document.getElementsByClassName('.foodAndPriceList')
             for (var i = 0; i < data.menu.length; i++) {
                 console.log(data.menu[i].name)
-            }
-
-            for (var i = 0; i < data.menu.length; i++) {
                 console.log(data.menu[i].price)
+                var apiInfoPassed = (data.menu[i].name + ' ' + data.menu[i].price)
+                console.log(apiInfoPassed)
+                document.querySelector('.foodAndPriceList').innerHTML += apiInfoPassed
             }
         })
 })
-
-fetch('https://galvanize-eats-api.herokuapp.com/menu')
-    .then((data) => data.json())
-    .then((data) => function getNames(data) {
-        for (var i = 0; i < data.menu.length; i++) {
-            console.log(data.menu[1].name)
-        }
-    })
