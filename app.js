e456f9b2f3923b444dbe09c8a2c04cabf81cfad9
andimalfresh <w.andrew.pedersen@gmail.com>
2018-09-28 11:20:27 -0600

    Working out event calls / form built / moving towards page wide communication

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 
 
-document.addEventListener("DOMContentLoaded", function printNames(pushNames) {
+document.addEventListener("DOMContentLoaded", function printNames() {
     fetch('https://galvanize-eats-api.herokuapp.com/menu')
         .then((data) => data.json())
         .then((data) => {
             for (var i = 0; i < data.menu.length; i++) {
-                console.log(data.menu[i].name)
-                console.log(data.menu[i].price)
-                var apiInfoPassed = (data.menu[i].name + ' ' + data.menu[i].price)
-                console.log(apiInfoPassed)
-                document.querySelector('.foodAndPriceList').innerHTML += apiInfoPassed
+                var apiInfoPassed = (data.menu[i].name + '   $' + data.menu[i].price)
+                var target = document.querySelector('.foodAndPriceList')
+                var button = document.createElement('button')
+                document.querySelector('button')
+                button.innerHTML = apiInfoPassed
+                target.parentNode.insertBefore(button, target.nextSibling)
+                button.setAttribute('class', 'apiButton')
+                button.setAttribute('value', data.menu[i].price)
+                button.setAttribute('name', data.menu[i].name)
+
             }
         })
+})
+
+
+
+var getButton = document.querySelectorAll('.apibutton')
+var onClick = getButton.addEventListener('click', function () {
+    console.log('Button was clicked')
+
+})
+
+
+var getFormSubmit = document.getElementById('#submitInfo')
+var customerInfoSaved = getFormSubmit.addEventListener('click', function () {
+    console.log('button clicked')
 })
