a8320ea933bc257315b353c79798bf62a723d443
andimalfresh <w.andrew.pedersen@gmail.com>
2018-09-28 13:18:16 -0600

    passing API Information around

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-document.addEventListener("DOMContentLoaded", function printNames() {
+function printNames() {
     fetch('https://galvanize-eats-api.herokuapp.com/menu')
         .then((data) => data.json())
         .then((data) => {
@@ -8,27 +8,36 @@ document.addEventListener("DOMContentLoaded", function printNames() {
                 var apiInfoPassed = (data.menu[i].name + '   $' + data.menu[i].price)
                 var target = document.querySelector('.foodAndPriceList')
                 var button = document.createElement('button')
-                document.querySelector('button')
+                // document.querySelector('button')
                 button.innerHTML = apiInfoPassed
                 target.parentNode.insertBefore(button, target.nextSibling)
                 button.setAttribute('class', 'apiButton')
                 button.setAttribute('value', data.menu[i].price)
                 button.setAttribute('name', data.menu[i].name)
+                button.addEventListener('click', function () {
+                    console.log(event.target.value)
+                    console.log(event.target.name)
+                    document.querySelector('.dropbox')
+                    var menuRow = document.createElement('p')
+                    document.querySelector('.dropbox').innerHTML += event.target.name + ' ' + event.target.value
+
+
+
+                })
 
             }
         })
-})
+}
+printNames()
 
 
+// var getButton = document.querySelectorAll('.apibutton')
+// console.log(getButton)
 
-var getButton = document.querySelectorAll('.apibutton')
-var onClick = getButton.addEventListener('click', function () {
-    console.log('Button was clicked')
-
-})
 
 
 var getFormSubmit = document.getElementById('#submitInfo')
-var customerInfoSaved = getFormSubmit.addEventListener('click', function () {
-    console.log('button clicked')
-})
+// var customerInfoSaved = getFormSubmit.addEventListener('click', function () {
+//     console.log('button clicked')
+// })
+
